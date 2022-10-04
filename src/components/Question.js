93bbdf1a54194c75296ea4d8764fffb960bf4f43
advaithca <advaith29042002@gmail.com>
2022-10-04 20:08:38 +0530

    Made it work

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,52 +1,17 @@
-import React, { useEffect, useState } from "react";
-import {nanoid} from "nanoid";
+import React from "react";
 
 export default function Question(props) {
-    const [optionArray, setOptionArray] = useState([])
-    const [selectAnswers, setSelectAnswers] = useState([])
-    function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
-        while (currentIndex != 0) {
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex--;
-          [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+    let optionsToDisplay = []
+    for (let i = 0; i < props.optionArray.length; i++){
+        if(i === props.id){
+            optionsToDisplay.push(props.optionArray[i])
         }
-        return array;
-      }
-    
-    useEffect(()=>{
-        let option = props.incorrects
-        option.push(props.answer)
-        let set = [...new Set(option)]
-        option = shuffle(set)
-        setOptionArray(option.map((option)=>(
-            <button key={nanoid()} name={props.id} value={option} dangerouslySetInnerHTML={{__html:  option}} onClick={answerSelector}/>
-        )))
-    },[])
-
-    function answerSelector(event) {
-        const name = event.target.name
-        const value = event.target.value
-        console.log(name,value)
-        setSelectAnswers((prevSelectAnswers)=>(
-            [
-                ...prevSelectAnswers,
-                {
-                    [name]:name,
-                    [value]:value
-                }
-            ]
-        ))
-
-        console.log(selectAnswers)
-      }
-      
+    }
     return (
         <div className="Question">
             <div className="question" dangerouslySetInnerHTML={{__html:  props.question}}/>
             <div className="options" >
-                {optionArray}
+                {optionsToDisplay}
             </div>
             <hr/>
         </div>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,32 +5,120 @@ import {nanoid} from "nanoid";
 export default function Quiz(props) {
     const [checkAnswers, setCheckAnswers] = useState(false)
     const [questions, setQuestions] = useState([])
+    const [marks, setMarks] = useState()
+    const [selectedAnswers, setSelectedAnswers] = useState([])
+    const [optionArray, setOptionArray] = useState([])
+    const [clicked, setClicked] = useState(false)
+
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+        while (currentIndex != 0) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+        return array;
+      }
+
+
+    function answerSelector(event) {
+        const name = event.target.name
+        const value = event.target.value
+        setClicked(previous=>!previous)
+        event.target.style.backgroundColor = clicked?"#4d5bff":""
+        setSelectedAnswers((prevSelectAnswers)=>{
+            let found = false
+            if(prevSelectAnswers.length>0){
+                for(let i = 0; i < prevSelectAnswers.length; ++i){
+                    if(prevSelectAnswers[i].question === name){
+                        prevSelectAnswers[i].answer = value
+                        found = true
+                        break
+                    }
+                }
+                if(!found){
+                    return [
+                        ...prevSelectAnswers,
+                        {
+                            question: name,
+                            answer: value
+                        }
+                    ]
+                }
+                return prevSelectAnswers
+            }
+            else {
+                return [
+                    {
+                        question: name,
+                        answer: value
+                    }
+                ]
+            }
+        })
+    }
+
     useEffect(()=>{
         fetch("https://opentdb.com/api.php?amount=5")
             .then(res => res.json())
-            .then(data => setQuestions(data.results))
-    },[])
+            .then(data => {
+                setQuestions(data.results)
+                setOptionArray(data.results.map((question)=>{
+                    let option = question.incorrect_answers
+                    option.push(question.correct_answer)
+                    let set = [...new Set(option)]
+                    option = shuffle(set)
+                    return option.map((option,index)=>{
+                        let buttonId = nanoid()
+                        return <button 
+                                    num={buttonId} 
+                                    key={nanoid()} 
+                                    id={index} 
+                                    name={question.question} 
+                                    value={option} 
+                                    dangerouslySetInnerHTML={{__html:  option}} 
+                                    onClick={answerSelector}
+                                    />
+                    })
+                }))
+            })
 
-    const QuestionsToDisplay = questions.map((question)=>(
+    },[])
+    
+    const QuestionsToDisplay = questions.map((question,index)=>(
         <Question
             question={question.question}
-            answer={question.correct_answer}
-            incorrects={question.incorrect_answers}
+            optionArray={optionArray}
+            id={index}
             key={nanoid()}
-            id={nanoid()}
         /> 
     ))
 
     function answer() {
         setCheckAnswers((previousCheckAnswers)=>(!previousCheckAnswers))
+        setMarks(()=>{
+            let marks = 0
+            for(let i = 0; i < questions.length; i++){
+                for(let j = 0; j < selectedAnswers.length; j++){
+                    if(questions[i].question === selectedAnswers[j].question){
+                        if(questions[i].correct_answer === selectedAnswers[j].answer){
+                            marks += 1
+                        }
+                    }
+                }
+            }
+            return marks
+        })
     }
+
     return (
         <div className="Quiz">
             <div className="Quiz--questions">
                 {QuestionsToDisplay}
             </div>
             {!checkAnswers && <button onClick={answer}>Check Answers</button>}
-            { checkAnswers && <div className="bottom">You got {2}/5 answers correct. <button onClick={props.handleClick}>Play Again</button></div>}
+            { checkAnswers && <div className="bottom">You got {marks}/5 answers correct. <button onClick={props.handleClick}>Play Again</button></div>}
         </div>
     )
 }
